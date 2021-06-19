f5f7ddee92d7f21fc30cdfb99ddb9591e4f3824f
Rituraj Jain <rrjain20122000@gmail.com>
2021-06-19 12:32:42 +0530

    added site

--- src/index.js
+++ src/index.js
@@ -1,31 +1,12 @@
-import initTilt from "./js/tilt";
-import initSr from "./js/sr";
-import resume from "./assets/resume.pdf";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-import "./style/main.scss";
+ReactDOM.render(<App />, document.getElementById("root"));
 
-$('a[href^="#"]').on("click", function (event) {
-  var target = $(this.getAttribute("href"));
-  if (target.length) {
-    event.preventDefault();
-    $("html, body").stop().animate(
-      {
-        scrollTop: target.offset().top,
-      },
-      1000
-    );
-  }
-});
-
-function addResume(pdf) {
-  if (!pdf) return;
-
-  const resumeButton = document.querySelector(".cta-btn--resume");
-  resumeButton.setAttribute("href", pdf);
-}
-
-initSr();
-initTilt();
-
-// uncomment this if you want to attach your resume
-addResume(resume);
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
