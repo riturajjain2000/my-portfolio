8c49d71659e5bfd4bba679e442c733b85b1ffc35
Rituraj Jain <rrjain20122000@gmail.com>
2021-08-02 13:19:18 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -226,10 +226,10 @@ const workExperiences = {
 /* Your Open Source Section to View Your Github Pinned Projects
 To know how to get github key look at readme.md */
 
-const openSource = {
-  showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
-  display: false // Set false to hide this section, defaults to true
-};
+// const openSource = {
+//   showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
+//   display: false // Set false to hide this section, defaults to true
+// };
 
 // Some big projects you have worked on
 
